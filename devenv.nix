b1e7f6b2c60ee478c371db9ab5a1be8f62fe49e8
Ahmet Cemal Özgezer <ozgezer@gmail.com>
2026-04-29 00:38:39 +0300

    feat(devenv): add initial dev‑environment configuration files
    Adds .envrc, .gitignore updates, and full `devenv.nix` with Rust setup
    plus `devenv.yaml` inputs for git‑hooks and rust‑overlay. This
    establishes a working dev environment with Rust tooling, git hooks, and
    basic scripts.

---
+++ devenv.nix
@@ -0,0 +1,81 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+    git
+    rustlings
+  ];
+
+  # https://devenv.sh/languages/
+  languages.rust = {
+    enable = true;
+    # https://devenv.sh/reference/options/#languagesrustchannel
+    channel = "stable";
+
+    components = [
+      "rustc"
+      "cargo"
+      "clippy"
+      "rustfmt"
+      "rust-analyzer"
+    ];
+    mold.enable = true;
+    version = "1.85.0";
+  };
+
+  # https://devenv.sh/processes/
+  processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = ''
+    echo hello from $GREET
+  '';
+
+  enterShell = ''
+    hello
+    git --version
+    rustc --version
+  '';
+
+  # https://devenv.sh/tasks/
+  tasks = {
+    #   "myproj:setup".exec = "mytool build";
+    #   "devenv:enterShell".after = [ "myproj:setup" ];
+    "rust:compile" = {
+      exec = "cargo build --release";
+      execIfModified = [
+        "src/**/*.rs"
+        "Cargo.toml"
+        "Cargo.lock"
+      ];
+    };
+  };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+  git-hooks.hooks = {
+    rustfmt.enable = true;
+    clippy.enable = true;
+  };
+
+  # See full reference at https://devenv.sh/reference/options/
+}
